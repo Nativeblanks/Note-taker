2116d90dea3f6fbc7734839f25264526b12016f9
Zach B <nativeblanks@gmail.com>
2021-10-17 20:28:11 -0400

    Added ability to add notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,45 @@
+//  Const
+
 const express = require("express")
 const app = express()
 const path = require("path")
 const fs = require("fs")
 const { response } = require("express")
 
+// Port number for local
 let PORT = process.env.PORT  || 8080
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 
+//  Get notes
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 })
 
 app.get("/api/notes", (req, res) => {
 fs.readFile("./db/db.json", "utf-8", (err, data)=>{
     if (err) {
-        console.log(err)
+        console.log(err);
     }else{
-        res.json(JSON.parse(data))
+        res.json(JSON.parse(data));
     }
 })
 })
+
+app.post("/api/notes", (req, res)=> {
+    req.body.id = Math.floor(Math.random() * 100);
+    let addNote = req.body
+
+    const savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+    console.log(savedNotes);
+    savedNotes.push(addNote);
+    
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.status(200).json({added: true});
+})
+
 
 
 
